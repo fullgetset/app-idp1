68ad0e3751fd6d1ec41a534fd79267c40ce4d6e9
Yuri Zaykov <ye.zaikou@vebtech.by>
2025-01-15 12:17:01 +0300

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import { Login } from "@components";
+
+const page = () => {
+  return <Login />;
+};
+
+export default page;
---
+++ src/components/login/login.component.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@components';
+
+const Login = () => {
+  return (
+    <div className='text-red-700'>
+      <Button>click me</Button>
+      login.component
+    </div>
+  );
+};
+
+export { Login };
